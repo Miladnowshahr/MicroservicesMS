5cbd86092a82a406d89ce92a6727f82a9b6ce4d7
Milad Nowshahr <miladnowshahr@yahoo.com>
2022-11-12 18:00:05 +0330

    CatalogApi, Implement Basket Redis

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.Api
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items = new();
+
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice => Items.Sum(s => s.Quantity * s.Price);
+    }
+}
---
+++ src/Services/Basket/Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _cache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache)) ;
+        }
+
+        public async Task DeleteBasket(string username)
+        {
+            await _cache.RemoveAsync(username);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await _cache.GetStringAsync(username);
+
+            if (string.IsNullOrEmpty(basket))
+                return null;
+             return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
+        {
+            await _cache.SetStringAsync(shoppingCart.UserName, JsonConvert.SerializeObject(shoppingCart));
+            return await GetBasket(shoppingCart.UserName);
+        }
+
+    }
+}
